ae54c70cab245091be639746ee4aeba0f05d1c8e
Rafael Honda <RafaHonda@MacBook-Pro-de-HONDA.local>
2019-02-24 11:27:37 -0300

    updated server port

--- app.js
+++ app.js
@@ -51,6 +51,7 @@ app.use('/', indexRoutes);
 app.use('/campgrounds', campgroundRoutes);
 app.use('/campgrounds/:id/comments', commentRoutes);
 
-app.listen(3000, function() {
+var port_number = server.listen(process.env.PORT || 3000);
+app.listen(port_number, function() {
   console.log('App listening on port 3000!');
 });
